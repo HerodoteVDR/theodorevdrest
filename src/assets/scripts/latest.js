fe04839ec08b4ccb645968d0eb3fed4e0f4ba18d
Théodore van der Rest <theodorevdrest@gmail.com>
2023-11-28 11:10:08 +0100

    hover card effect

--- src/assets/scripts/latest.js
+++ src/assets/scripts/latest.js
@@ -1,8 +1,24 @@
-var cards = document.getElementsByClassName('c-latest__item')
+var cards = document.getElementsByClassName('c-latest__item');
+console.log(cards);
 
 for (let i = 0; i < cards.length; i++) {
-	const card = array[i];
-	card.addEventListener('mouseover', function() {
-		console.log("mouseover")
-	})
+    const card = cards[i]; // Utilisez "cards" au lieu de "array"
+    card.addEventListener('mouseover', function() {
+		onCardHover(card, cards);
+	});
+}
+
+function onCardHover(ccard, ccards) {
+	for (let i= 0; i < ccards.length; i++) {
+		const thisCard = ccards[i];
+		if(thisCard.classList.contains('selected')) {
+			thisCard.classList.add('previous');
+		}
+		else {
+			thisCard.classList.remove('previous');
+		}
+		thisCard.classList.remove('selected')	
+	}	
+
+	ccard.classList.add('selected')
 }
